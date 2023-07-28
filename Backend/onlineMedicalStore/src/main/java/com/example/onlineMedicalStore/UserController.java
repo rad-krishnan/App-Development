9e86fe70da05e8d172812e7f4e96d140418a6827
Radhakrishnan B <100069355+rad-krishnan@users.noreply.github.com>
2023-07-28 12:09:29 +0530

    Add files via upload

---
+++ Backend/onlineMedicalStore/src/main/java/com/example/onlineMedicalStore/UserController.java
@@ -0,0 +1,30 @@
+package com.example.onlineMedicalStore;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/users")
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+    
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@RequestBody User newUser) {
+        return userService.registerUser(newUser);
+    }
+}
---
+++ Front End/src/SignUpForm.js
@@ -0,0 +1,138 @@
+import React, { useState } from 'react';
+import './SignUpForm.css';
+import { Link } from "react-router-dom";
+import axios from 'axios';
+import "./SignUpForm.css"
+
+const SignUpForm = () => {
+  const [name, setName] = useState('');
+  const [username, setUsername] = useState('');
+  const [mobileNumber, setMobileNumber] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+  const [passwordMatch, setPasswordMatch] = useState(true);
+  const [showPopup, setShowPopup] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (password === confirmPassword) {
+      // Password and Confirm Password match
+      // Perform form submission logic here
+
+      // Create user object
+    const user = {
+      name,
+      username,
+      mobileNumber,
+      email,
+      password,
+      confirmPassword
+    };
+    
+    // Send a post request to the back-end
+    axios
+      .post('http://localhost:8080/register', user) 
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err));
+
+    } else {
+      // Password and Confirm Password do not match
+      setPasswordMatch(false);
+      setShowPopup(true);
+    }
+  };
+
+  const handleTogglePassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  return (
+    <div className="signup-form-container">
+      <h2>Sign Up</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="mobile-number">Mobile Number:</label>
+          <input
+            type="text"
+            id="mobile-number"
+            value={mobileNumber}
+            onChange={(e) => setMobileNumber(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <div className="password-field">
+            <input
+              type={showPassword ? 'text' : 'password'}
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+            <button
+              type="button"
+              className="toggle-password"
+              onClick={handleTogglePassword}
+            >
+              {showPassword ? 'Hide' : 'Show'}
+            </button>
+          </div>
+        </div>
+        <div className="form-group">
+          <label htmlFor="confirm-password">Confirm Password:</label>
+          <input
+            type="password"
+            id="confirm-password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            required
+          />
+          {!passwordMatch && showPopup && (
+            <p className="password-match-error">Passwords do not match.</p>
+          )}
+        </div>
+          <button type="submit">Create Account</button>
+          <p> Already have an account? 
+          <Link to="/" className="link3">
+          <strong> <center>Back to Login</center></strong>
+          </Link>
+          </p>
+      </form>
+    </div>
+  );
+};
+
+export default SignUpForm;
