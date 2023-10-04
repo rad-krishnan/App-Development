cdb2d2fe194719d57a0a00ada466cd65345c3ec5
Radhakrishnan B <100069355+rad-krishnan@users.noreply.github.com>
2023-10-04 22:58:02 +0530

    Add files via upload

---
+++ Micro Service/Microservice/Client_Feedback/src/main/java/com/example/Client_Feedback/ClientFeedbackApplication.java
@@ -0,0 +1,14 @@
+package com.example.Client_Feedback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.sOpringframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientFeedbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientFeedbackApplication.class, args);
+	}
+
+}
